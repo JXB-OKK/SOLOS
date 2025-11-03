a76fd5ee6ef24a8c5e1da080e33712a7f9d52890
JXB-OKK <xbjvve@gmail.com>
2025-11-03 20:52:22 +0100

    Update SOLO.py

--- SOLO.py
+++ SOLO.py
@@ -1,2 +1,8 @@
+# مثال بسيط إذا تريد استيراد ملف آخر وناديه أوتوماتيكياً
 if __name__ == "__main__":
-    iAmMain().iAmMenu()
+    try:
+        from HEXX import iAmMain
+        iAmMain().iAmMenu()
+    except Exception:
+        print("فشل استيراد/تشغيل HEXX.iAmMain().iAmMenu()")
+        import traceback; traceback.print_exc()
