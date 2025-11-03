07079f18146658834f0f120b70abd3ae7f5aaa28
JXB-OKK <xbjvve@gmail.com>
2025-11-03 21:08:14 +0100

    Update SOLO.py

--- SOLO.py
+++ SOLO.py
@@ -1,5 +1,5 @@
 # SOLO.py
-from HEXX import iAmMain   # استيراد الكلاس من الملف HEXX.cpython-312.so
+from HEXX import iAmMain   # استيراد الكلاس من ملف HEXX.cpython-312.so
 
 if __name__ == "__main__":
-    iAmMain().iAmMenu()    # استدعاء الميثود داخل الكلاس
+    ()iAmMain().iAmMenu    # تشغيل الميثود داخل الكلاس
