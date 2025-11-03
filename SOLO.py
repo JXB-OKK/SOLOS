07167eb72095bde14526658088a33b9aae3b4041
JXB-OKK <xbjvve@gmail.com>
2025-11-03 20:47:09 +0100

    Update SOLO.py

--- SOLO.py
+++ SOLO.py
@@ -1,7 +1,4 @@
-class iAmMain:
-    def iAmMenu(self):
-        print("القائمة اشتغلت ✅")
+from SOLO import iAmMain
 
-# شغله فقط عندما يُنفذ مباشرة
 if __name__ == "__main__":
     iAmMain().iAmMenu()
